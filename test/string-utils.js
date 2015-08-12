b42ace2ce2d534f8f384d77e9c4f24c840c231ee
Travis Fischer <fisch0920@gmail.com>
2015-08-12 04:49:40 -0400

    .

---
+++ test/string-utils.js
@@ -0,0 +1,12 @@
+var test = require('tape')
+
+var StringUtils = require('../lib/string-utils')
+
+test('timestamp', function (t) {
+  var timestamp = StringUtils.timestamp()
+  t.ok(timestamp)
+  t.equal(typeof timestamp, 'string')
+  t.equal(typeof +timestamp, 'number')
+  t.ok(new Date(+timestamp) < new Date())
+  t.end()
+})
