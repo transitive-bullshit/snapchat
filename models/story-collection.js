b202fe1eaa7b7bc466afebc87be4ab279e12968a
Travis Fischer <fisch0920@gmail.com>
2015-08-17 06:09:55 -0400

    .

--- models/story-collection.js
+++ models/story-collection.js
@@ -13,22 +13,32 @@ function StoryCollection (params) {
 
   var thumbs = params['thumbnails']
 
+  // The username of the user associated with this story collection.
   self.username = params['username']
+  // Whether this story contains explicit content.
   self.matureContent = !!params['mature_content']
-  self.adPlacementData = params['ad_placement_metadata']
 
+  // Only applies to shared stories.
+  self.adPlacementData = params['ad_placement_metadata']
+  // The display name of the shared story. (shared only)
   self.displayName = params['display_name']
+  // The identifier of the shared story. (shared only)
   self.sharedIdentifier = params['shared_id']
+  // Whether the shared story is local or not. (shared only)
   self.isLocal = !!params['is_local']
 
+  // Only applies to shared stories.
   if (thumbs) {
+    // The thumbnail for the viewed state of the story.
     self.viewedThumbnail = thumbs['viewed']['url']
+    // The thumbnail for the unviewed state of the story.
     self.unviewedThumbnail = thumbs['unviewed']['url']
 
     self.viewedThumbnailNeedsAuth = !!thumbs['viewed']['needs_auth']
     self.unviewedThumbnailNeedsAuth = !!thumbs['unviewed']['needs_auth']
   }
 
+  // An array of Story objects.
   self.stories = (params.stories || [ ]).map(function (story) {
     return new Story(story)
   })
